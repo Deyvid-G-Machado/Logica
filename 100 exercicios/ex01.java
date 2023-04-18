bd56dadc45471fa2f757824aa6a292277914bcb8
Deyvid-G-Machado <deyvid.zimba@hotmail.com>
2023-04-18 18:51:38 -0300

    Adicionado o exercicio 1

---
+++ 100 exercicios/ex01.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ex01 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Para calcular a base e a altura do retangulo");
+		
+		System.out.println("Digite a base:");
+		float base = input.nextFloat();
+		
+		System.out.println("Digite a altura:");
+		float altura = input.nextFloat();
+		
+		System.out.println("Area: " + base * altura);
+		System.out.println("Perimetro: " + 2*(base + altura));
+
+	}
+
+}
---
+++ 100 exercicios/ex01.py
@@ -0,0 +1,8 @@
+print('Para calcular a base e a altura do retangulo')
+
+base = float(input('Digite a base: '))
+altura = float(input('Digite a altura: '))
+print()
+
+print('Area:' , base * altura)
+print('perimetro:' , 2*(base+altura))
