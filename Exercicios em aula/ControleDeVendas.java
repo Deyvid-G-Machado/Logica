be6c78fce7ee16299904a44c96dfed4e31bedc30
Deyvid-G-Machado <deyvid.zimba@hotmail.com>
2023-05-22 18:19:47 -0300

    Modificações no ControleDeVendas

--- Exercicios em aula/ControleDeVendas.java
+++ Exercicios em aula/ControleDeVendas.java
@@ -1,20 +1,21 @@
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
 
-public class ControleDeVendas {
+public class Main {
 	// Escopo global (enxergado por todas as funções)
 	public static Scanner input = new Scanner(System.in);
 	public static List<String> nomeProdutos = new ArrayList<>();	// Onde fica guarddo os nomes dos produtos
 	public static List<Double> valorProdutos = new ArrayList<>();	// Onde fica guardado os valores do produto
 	public static List<String> vendas = new ArrayList<>();			// Aqui ira ficar guardado todas as vendas feitas
 	public static double vendasTotal = 0;
-	public static int opcao;
+	public static int opcao, indiceProduto;
 	
 	public static void main(String[] args) {
 		// Menu de escolhas
 		boolean menuPrincipal = true, menuProdutos;
-		
 		
 		while (menuPrincipal) {
 			System.out.println("=====| Controle de Vendas |=====\n"
@@ -71,15 +72,15 @@ public class ControleDeVendas {
 				break;
 			case 3: // Alterar
 				System.out.println("Qual Produto deseja alterar (digite o indice referente ao produto):");
-				opcao = input.nextInt() - 1;
+				indiceProduto = input.nextInt() - 1;
 				
-				alterarProdutos(opcao);
+				alterarProdutos();
 				break;
 			case 4: // Remover
 				System.out.println("Qual Produto deseja remover (digite o indice referente ao produto):");
-				opcao = input.nextInt() - 1;
+				indiceProduto = input.nextInt() - 1;
 				
-				removerProdutos(opcao);
+				removerProdutos();
 				break;
 			case 0: // Voltar para o menu principal
 				menuProdutos = false;
@@ -108,7 +109,7 @@ public class ControleDeVendas {
 	}
 	
 	// Alterar produtos
-	public static void alterarProdutos(int opcao) {
+	public static void alterarProdutos() {
 		boolean menuAlterar = true;
 		
 		while (menuAlterar) {
@@ -120,14 +121,14 @@ public class ControleDeVendas {
 			case 1: // Nome
 				System.out.println("Escolha um novo nome:");
 				String nome = input.next() ;
-				nomeProdutos.set(opcao, nome);
+				nomeProdutos.set(indiceProduto, nome);
 				
 				System.out.println("Nome do produto alterado!");
 				break;
 			case 2: // Valor
 				System.out.println("Escolha um novo valor:");
 				Double valor = input.nextDouble();
-				valorProdutos.set(opcao, valor);
+				valorProdutos.set(indiceProduto, valor);
 				
 				System.out.println("Valor do produto alterado!");
 				break;
@@ -143,8 +144,8 @@ public class ControleDeVendas {
 	}
 	
 	// Remover produtos
-	public static void removerProdutos(int opcao) {
-		nomeProdutos.remove(opcao);
+	public static void removerProdutos() {
+		nomeProdutos.remove(indiceProduto);
 		valorProdutos.remove(opcao);
 		System.out.println("Produto removido!");
 	}
@@ -164,14 +165,15 @@ public class ControleDeVendas {
 			switch (opcao) {
 			case 1: // Cadastro venda
 				System.out.println("Escolha o produto:");
-				int indiceProduto = input.nextInt() - 1;
+				indiceProduto = input.nextInt() - 1;
 				
 				System.out.println("Escolha a quantidade:");
 				int quantidadeProduto = input.nextInt();
 				
-				cadastrarVenda(indiceProduto, quantidadeProduto);
+				cadastrarVenda(quantidadeProduto);
 				break;
 			case 2: // Relatorio de vendas
+				System.out.println("Relatorio de vendas:");
 				relatorioVendas();
 				break;
 			case 3: // Lista de produtos
@@ -189,7 +191,7 @@ public class ControleDeVendas {
 	}
 	
 	// Cadastrar venda
-	public static void cadastrarVenda(int indiceProduto, int quantidadeProduto) {
+	public static void cadastrarVenda(int quantidadeProduto) {
 		if (indiceProduto < nomeProdutos.size() && indiceProduto >= 0) {
 			for (int i = 0; i < quantidadeProduto; i++) {
 				vendas.add(nomeProdutos.get(indiceProduto) + " | " + valorProdutos.get(indiceProduto));
@@ -204,7 +206,6 @@ public class ControleDeVendas {
 	
 	// Relatorio vendas
 	public static void relatorioVendas() {
-		System.out.println("Relatorio de vendas:");
 		for (int i = 0; i < vendas.size(); i++) {
 			System.out.println(vendas.get(i));
 		}
@@ -212,9 +213,20 @@ public class ControleDeVendas {
 	
 	// Fechamento de caixa
 	public static void fechamentoCaixa() {
-		System.out.println("=====| Fechamento de Caixa |=====");
+		Date dataHoraAtual = new Date();
+		String data = new SimpleDateFormat("dd/MM/yyyy").format(dataHoraAtual);
+		String hora = new SimpleDateFormat("HH:mm:ss").format(dataHoraAtual);
+		
+		System.out.println("=====| Fechamento de Caixa |=====\n"
+				+ "Data: " + data + "\nHora: " + hora);
+		System.out.println("\nRelatorio de vendas:");
+		
 		relatorioVendas();
 		System.out.println("\nTotal: R$ " + vendasTotal + "\n");
+		
+		// Limpar o ArrayList "vendas" e retorna novamente a 0 a variavel vendasTotal
+		vendas.clear();
+		vendasTotal = 0;
 	}
 
 }
